a74eadc80c13c5c210abe2f6e3b35d807ebb447b
defauth98 <neto.daniribeiro@gmail.com>
2021-08-17 07:54:54 -0300

    feat: limpando a estrutura e instalando o bootstrap

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import Card from '../components/Card'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Card/>
+    </div>
+  )
+}
+
+export default Home
